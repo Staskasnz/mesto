547bc0bdeecefaa17e44d35f39c3cb82dd662ba0
Stanislav Alexandrov <staskasnz@mail.ru>
2022-12-08 22:17:26 +0300

    second

--- skripts/Index.js
+++ skripts/Index.js
@@ -1,29 +1,31 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
-let inputName = document.querySelector('.popup__input-name');
-let inputVocation = document.querySelector('.popup__input-vocation');
+let inputName = document.querySelector('.popup__input_type_name');
+let inputVocation = document.querySelector('.popup__input_type_vocation');
 let profileName = document.querySelector('.profile__name');
 let profileVocation = document.querySelector('.profile__vocation');
 let saveButton = document.querySelector('.popup__save-button');
 let popupForm = document.querySelector('.popup__form');
 
 editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupOpen);
-popupForm.addEventListener('submit', popupSave); 
+closeButton.addEventListener('click', popupClose);
+popupForm.addEventListener('submit', popupSave);
 
-function popupOpen(){
-    popup.classList.toggle('popup_opened');
-    if (popup.classList.contains('popup_opened')){
-        inputName.value = profileName.textContent;
-        inputVocation.value = profileVocation.textContent;
-    }
+function popupOpen() {
+    popup.classList.add('popup_opened');
+    inputName.value = profileName.textContent;
+    inputVocation.value = profileVocation.textContent;
 }
+
+function popupClose() {
+    popup.classList.remove('popup_opened');
+}
+
 
 function popupSave(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileVocation.textContent = inputVocation.value;
-    popupOpen();
+    popupClose();
 }
-
